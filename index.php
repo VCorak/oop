681cb265ef4d65c0e7a88dc4bb2edfa48b3d03fb
Valentina Corak <valentinacorak@gmail.com>
2021-05-26 11:39:10 +0200

    Solution to first exercise

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+
+// First exercise
+
 class Beverage {
-    
-}
+     // Properties
+   var string $color;
+   var float $price;
+   var string $temperature = 'cold';
+
+   public function __construct(string $color, float $price, string $temperature = "cold") {
+       $this->color = $color;
+       $this->price = $price;
+       $this->temperature = $temperature;
+   }
+     // Methods
+
+   public function getInfo() {
+         return "This beverage is " . $this->temperature . " and " . $this->color;
+     }
+ }
+
+ $cola = new Beverage("black", 2);
+
+ echo $cola->getInfo();
+
+
+
+
+
