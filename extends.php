a56437577484d55c7db6192d563ba11fae2730f4
Valentina Corak <valentinacorak@gmail.com>
2021-05-27 12:35:34 +0200

    Testing

--- extends.php
+++ extends.php
@@ -1,4 +1,29 @@
 <?php
+class Beverage {
+    // Properties
+    var string $color;
+    var float $price;
+    var string $temperature = 'cold';
+
+    public function __construct(string $color, float $price, string $temperature = "cold") {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+    // Methods
+
+    public function getInfo() {
+        return "This beverage is " . $this->temperature . " and is " . $this->color . " " ;
+    }
+}
+
+$cola = new Beverage("black", 2);
+
+/*echo $cola->getInfo();
+echo "<br>";
+echo $cola->temperature;*/
+
+
 
 // Second exercise
 
@@ -34,6 +59,10 @@ return parent::getInfo() . "and has a " . $this->alcoholpercentage  . "% alcohol
 
 $duvel = new Beer("blond", 3.5, "Duvel", 8.5);
 
-echo $duvel->getInfo().PHP_EOL;
+echo $duvel->getInfo();
+echo "<br>";
+echo $duvel->alcoholpercentage;
+echo "<br>";
+echo $duvel->getAlcoholpercentage();
 
-echo $duvel->getAlcoholpercentage().PHP_EOL;
+
